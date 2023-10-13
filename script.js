0c73d4d698b33b847f7d44100bb6377e187caa7d
AteshanGunduz <145977486+AteshanGunduz@users.noreply.github.com>
2023-10-13 11:32:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+const age = 15;
+if (age >= 15 ) {
+   console.log("You can drive !")
+} else {
+   console.log("Too young to drive")
+}
+
+
+const drinkSize = "large";
+
+switch(drinkSize){
+  case "small":
+      console.log("$5");
+      break
+  case "medium":
+      console.log("$7");
+      break
+  case "large":
+      console.log("$8")
+      break
+  default:
+      console.log("choose a size");     
+}
